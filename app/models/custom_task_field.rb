fd42cf50106373562c6ad38a448f070c4d472f40
freemanoid <freemanoid321@gmail.com>
2013-09-08 16:04:13 +0300

    Add validations for models.

--- app/models/custom_task_field.rb
+++ app/models/custom_task_field.rb
@@ -3,4 +3,6 @@ class CustomTaskField < ActiveRecord::Base
 
   belongs_to :sprints_tasks
   belongs_to :custom_task_field_type
+
+  validates_presence_of :custom_task_field_type_id, :sprints_tasks_id
 end
--- app/models/custom_task_field_type.rb
+++ app/models/custom_task_field_type.rb
@@ -2,4 +2,5 @@ class CustomTaskFieldType < ActiveRecord::Base
   unloadable
 
   has_many :custom_task_fields
+  validates :name, presence: true, uniqueness: true
 end
