c6c1a4d183109afd6a885aa86bbc2145e3274d9e
freemanoid <freemanoid321@gmail.com>
2013-09-09 01:49:04 +0300

    Fix for custom relation name.

--- app/models/custom_task_field_type.rb
+++ app/models/custom_task_field_type.rb
@@ -3,7 +3,7 @@ class CustomTaskFieldType < ActiveRecord::Base
 
   attr_accessible :name
 
-  has_many :custom_task_fields, dependent: :destroy
+  has_many :custom_task_fields, dependent: :destroy, foreign_key: :type_id
 
   validates :name, presence: true, uniqueness: true
 
