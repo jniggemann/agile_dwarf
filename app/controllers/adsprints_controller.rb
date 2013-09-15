f4f4b743ac21f1c99a384e2abebafe561afad2ae
freemanoid <freemanoid321@gmail.com>
2013-09-15 15:15:50 +0300

    Show custom fields in created tasks and in new tasks.

--- app/controllers/adsprints_controller.rb
+++ app/controllers/adsprints_controller.rb
@@ -20,6 +20,7 @@ class AdsprintsController < ApplicationController
     @project_id = @project.id
     @plugin_path = File.join(Redmine::Utils.relative_url_root, 'plugin_assets', 'agile_dwarf')
     @closed_status = Setting.plugin_agile_dwarf["stclosed"].to_i
+    @available_custom_fields = SprintsTasks.available_custom_fields(@project)
   end
 
   private
